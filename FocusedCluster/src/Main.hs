f0bfb404f3f0ba1e51bb846ee43fd345f3d82c9c
Gideon Providence <jprovidence@live.ca>
2011-10-28 14:56:59 -0400

    main for trial

--- FocusedCluster/src/Main.hs
+++ FocusedCluster/src/Main.hs
@@ -11,6 +11,8 @@ import Viterbi
 
 stringOfText = "The Viterbi Algorithm was originally concocted to separate signal from noise in telecom lines. Or something. It happened before I was born, so I’m just taking wikipedia’s word for it. It’s all about probabilities, but I think it can be explained simply by thinking of drunken tourists dancing a conga line. Imagine you were walking past a conga line (you’re both going in the same direction) trying to guess the probability that each person you passed would trip over and get blood all over your Hawaiian shirt. Easy! Of course you could fathom a pretty close guess based on how many Rum’n Cokes that particular person had consumed. The teetotaler probably  has a 10% chance of tripping, whereas the guy with an empty mini-keg as a hat is pushing 60%. We can call these percentages the ‘emission probability’, or the emission of blood onto shirt probability."
 
-main =
-    putStrLn $ show "hello"
+main = do
+    vit <- trainVit
+    nouns <- pullNouns vit $ words stringOfText
+    putStrLn $ show $ L.map B.unpack nouns
 
